6563d2379b4a2f40a63af451ecf206234b8699e8
David Stavis <davidstavis@gmail.com>
2022-10-16 10:28:53 -0600

    Install googleMaps package, create Map component, and display default google map on browser

---
+++ src/components/Map/Map.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Map.css";
+import { Loader } from "@googlemaps/js-api-loader"
+
+const mapsLoader = new Loader({
+  apiKey: "AIzaSyDSQlcJ_1hY3ihSkHu9VEGm5kcWtTYNd5k",
+  // version: "weekly",
+  // ...additionalOptions,
+});
+
+mapsLoader.load().then((google) => {
+  let map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+  console.log(map)
+});
+
+const Map = () => {
+  return (
+    <div id="map">
+
+    </div>
+  )
+};
+
+export default Map;
