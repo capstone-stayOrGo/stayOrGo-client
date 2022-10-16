bef435a06c51b3e4ecd2c135ad70b2cffc62e599
David Stavis <davidstavis@gmail.com>
2022-10-16 11:29:37 -0600

    Make a marker display

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -13,7 +13,10 @@ mapsLoader.load().then((google) => {
     center: { lat: -34.397, lng: 150.644 },
     zoom: 8,
   });
-  console.log(map)
+  const marker = new google.maps.Marker({
+    position: { lat: -34.397, lng: 150.644 },
+    map: map,
+  });
 });
 
 const Map = () => {
