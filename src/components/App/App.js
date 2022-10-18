4c9ac284874d07d1130a2bea0040d40ed8f1f602
Shauna MacFarlane-Okongo <102887963+DrSLMac@users.noreply.github.com>
2022-10-18 17:05:20 -0500

    add files for each container on page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,31 +1,33 @@
 // Dependencies
 import React, { useState, useEffect } from "react"
-import { GenIcon } from "react-icons";
+// import { GenIcon } from "react-icons";
 // Components
 import Footer from '../Footer/Footer';
 import Map from '../Map/Map';
+import NavBar from "../NavBar/NavBar";
 import './App.css';
 
 function App() {
 
-  const [location, setLocation] = useState(undefined)
+  const [location, setLocation] = useState(null)
 
   useEffect( () => {
     navigator.geolocation.getCurrentPosition( (geoposition) => {
       const {latitude, longitude} = geoposition.coords
-      setLocation({lat: latitude, long: longitude})
+      setLocation({lat: latitude, lng: longitude})
     })
   }, [])
 
   useEffect(()=> {
-    console.log(location)
+    // Runs whenever the location changes
+    // console.log(location)
   }, [location])
 
   return (
     <div className="App">
-      <h1>Stay or Go</h1>
-      <div id="map"></div>
-      <Map />
+      <NavBar />
+      <h1 className="title">Stay or Go</h1>
+      {location ? <Map location={location} /> : <h2>Loading...</h2>}
       <Footer />
     </div>
   );
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './NavBar.css'
+
+const NavBar = () => {
+    return (
+        <nav className='navbar'>
+            Stay or Go
+        </nav>
+    )
+}
+
+export default NavBar
