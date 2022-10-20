ba3f873f144af3105d7c8cc50a78c05cd9857426
Shauna MacFarlane-Okongo <102887963+DrSLMac@users.noreply.github.com>
2022-10-20 01:07:06 -0500

    add phone number input ability

--- src/components/SMSNotification/SMSNotification.js
+++ src/components/SMSNotification/SMSNotification.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
+import PhoneInput from 'react-phone-input-2'
+import 'react-phone-input-2/lib/style.css'
 import './SMSNotification.css'
 
 const SMSNotification = () => {
+    const [ phoneNumber, setPhoneNumber ] = useState();
+
     return (
         <section className='sms-notify'>
             <p>SMS Notification sign up</p>
+            <PhoneInput
+                country={'us'}
+                value={phoneNumber}
+                // onChange={phoneNumber => setPhoneNumber({ phoneNumber })}
+            />
+            <button>Submit</button>
         </section>
     )
 }
