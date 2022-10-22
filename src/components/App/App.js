48928d84242e0d3103438ed9c93a16c1bf28ccde
David Stavis <davidstavis@gmail.com>
2022-10-21 21:16:32 -0600

    Swap postman mock server disaster URL with actual disaster URL. Save postman url in separate variable for future use. Remove commented-out code temporarily for readability.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -19,50 +19,34 @@ function App() {
   const [forecast, setForecast] = useState("")
   const [disasterAlerts, setDisasterAlerts] = useState("")
 
+  const formDisasterURLWithCoords = () => {
+    // Should we move this functionality into api.js?
+    return `${DISASTER_API_URL}?lat=${location.lat}&long=${location.lng}`
+  }
+
   const fetchDisasterData = async () => {
-    const response = await fetch("https://dc6e72c4-8622-4280-9089-79102851df02.mock.pstmn.io/api/v1/disasters?lat=33.2896&long=-97.6982");
+    // Should we move this functionality into api.js?
+    const urlForDisastersNearMe = formDisasterURLWithCoords()
+    const response = await fetch(urlForDisastersNearMe);
     return await response.json();
   }
-  // const [lat, lng] = location.split(" ")
-  // const fetchCurrentWeather = async (location) => {
-  //   // const [lat, lng] = location
-  //   const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lng}&appid=${WEATHER_API_KEY}&units=imperial`);
-  //   const forecastFetch = fetch(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lng}&appid=${WEATHER_API_KEY}&units=imperial`);
-    
-  //   Promise.all([ currentWeatherFetch, forecastFetch ])
-  //       .then(async (response) => {
-  //         const weatherResponse = await response[0].json();
-  //         const forecastResponse = await response[1].json();
-  //         console.log('weatherResponse: ', weatherResponse)
-  //         // setCurrentWeather({city: location, ...weatherResponse})
-  //         // setForecast({city: location, ...forecastResponse})
-  //       })
-  //       .catch((err) => console.log(err))
-  // // const disasterFetch = fetch(`${DISASTER_API_URL}`)
-  //     }
+  
 
   useEffect( () => {
     navigator.geolocation.getCurrentPosition( (geoposition) => {
       const {latitude, longitude} = geoposition.coords
       setLocation({lat: latitude, lng: longitude})
     })
-    fetchDisasterData()
-    // fetchCurrentWeather()
-    .then((data) => setDisasterAlerts(data.data))
   }, [])
   
- 
-  console.log('location: ', location) //location: {lat: 32.9956027, lng: -97.3765712}
-  // console.log("currentWeather: ", currentWeather)
-  // console.log("forecast: ", forecast)
-  console.log("disasterAlerts: ", disasterAlerts)
-
-  // useEffect( () => {
-  //   navigator.geolocation.getCurrentPosition( (geoposition) => {
-  //     const {latitude, longitude} = geoposition.coords
-  //     setLocation({lat: latitude, lng: longitude})
-  //   })
-  // }, [])
+  useEffect( () => {
+    if(location.lat && location.lng){
+      fetchDisasterData()
+          .then((data) => {
+            setDisasterAlerts(data.data)
+          })
+    }
+  }, [location])
 
   return (
     <div className="App">
@@ -71,8 +55,7 @@ function App() {
         <div className="sms-search-weather">
           <SMSNotification />
           <WeatherAlert disasterAlert={disasterAlerts}/>
-          {/* <Search onSearchChange={handleOnSearchChange}/> */}
-          {/* {currentWeather && location ? <Weather data={currentWeather} location={location} /> : <h2>Loading...</h2>} */}
+          
         </div>
         <div className="map-alert">
           {location ? <Map location={location} /> : <h2>Loading...</h2>}
@@ -89,45 +72,3 @@ export default App;
 
 
 
-
-// const onLoad = (location) => {
-  // const [lat, lon] = location.split(" ")
-  // const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=imperial`)
-  // const forecastFetch = fetch(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=imperial`)
-  // const disasterFetch = fetch(`${DISASTER_API_URL}`)
-  // //  
-  // Promise.all([ currentWeatherFetch, forecastFetch, disasterFetch ])
-  // .then(async (response) => {
-  //   const weatherResponse = await response[0].json();
-  //   const forecastResponse = await response[1].json();
-  //   const disasterResponse = await response[2].json();
-  
-  //   setCurrentWeather({ ...weatherResponse });
-  //   setForecast({ ...forecastResponse });
-  //   setDisasterAlerts({ data: location , ...disasterResponse })
-  // })
-  // .catch((err) => console.log(err))
-// }
-
-
-
-  // const handleOnSearchChange = (searchData) => {
-  //   // console.log("searchData: ", searchData) //searchData: {value: '42.558333333 1.555277777', label: 'Andorra, AD'} from search function
-  //   // const [lat, lon] = searchData.value.split(" ")
-    
-  //   // const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=imperial`)
-  //   // const forecastFetch = fetch(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=imperial`)
-  //   // const disasterFetch = fetch(`${DISASTER_API_URL}`)
-
-  //   // Promise.all([currentWeatherFetch, forecastFetch, disasterFetch ])
-  //   // .then(async (response) => {
-  //   //   const weatherResponse = await response[0].json();
-  //   //   const forecastResponse = await response[1].json();
-  //   //   const disasterResponse = await response[2].json();
-
-  //   //   setCurrentWeather({ city: searchData.label , ...weatherResponse });
-  //   //   setForecast({ city: searchData.label , ...forecastResponse });
-  //   //   setDisasterAlerts({ data: searchData , ...disasterResponse })
-  //   // })
-  //   // .catch((err) => console.log(err))
-  // }
