9edddc9ed8dbbcb96f9806cd9a0d90d13b3418d0
Shauna MacFarlane-Okongo <102887963+DrSLMac@users.noreply.github.com>
2022-10-31 15:23:19 -0500

    add testing coverage

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,9 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
+      require('@cypress/code-coverage/task')(on, config)
       // implement node event listeners here
+      return config
     },
   },
 });
